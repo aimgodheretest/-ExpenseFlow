e6d04309dcf12a24b390dea5d35901ffbeba5363
Murli Kumar <aimgodhere@gmail.com>
2026-07-28 18:05:46 +0530

    refactor(auth): move password hashing to mongoose pre-save hook

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -26,14 +26,11 @@ export const signup = async (req, res) => {
       });
     }
 
-    // Hash password
-    const hashedPassword = await bcrypt.hash(password, 10);
-
     // Create user
     const user = await User.create({
       name,
       email: email.toLowerCase(),
-      password: hashedPassword,
+      password,
     });
 
     res.status(201).json({
